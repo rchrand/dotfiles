169c9cfbb53217273e958e2f9d3785fc43ef4ec1
Rune Andersen <Rune.andersen92@gmail.com>
2013-07-30 15:20:28 +0200

    Tested the script in Vagrant and needed to make a few changes, but it should work now

--- init.py
+++ init.py
@@ -4,14 +4,16 @@ cwd = os.getcwd();
 HOME = "/home/" + getpass.getuser() + "/"
 
 for files in os.listdir(cwd):
-    if os.path.isdir(files) and files == ".git" and files == "init.py":
-        shutil.copytree(cwd + "/" + files, HOME)
+    if os.path.isdir(files) and files == ".git" and files == "init.py": 
+        shutil.copytree(cwd + "/" + files, HOME + "/" files)
     else:
         shutil.copy(cwd + "/" + files, HOME)
 
-print ("Remember:
-1: to run :BundleInstall inside vim to install plugins 
-2: You also need to compile YouCompleteMe inside the dicrtory. 
-3: Remove the PASSWORD from your Irssi settings
-4: Install Zsh & OhMyZsh, URxvt (To match .Xdefaults), Tmux, Mpd & ncmpcpp, Compile Vim from source with Python support (YouCompleteMe), Emacs if needed ")
+text = """Remember to:
+1: Install Zsh & OhMyZsh, URxvt (To match .Xdefaults), Tmux, MPD & ncmpcpp, Irssi, Compile Vim from source with Python support (YouCompleteMe), Emacs if needed
+2: to run :BundleInstall inside vim to install plugins 
+3: You also need to compile YouCompleteMe inside the dicrtory. 
+4: Remove the PASSWORD from your Irssi settings """
+
+print (text)
 
