b17a864db761d7b5c19e103a5e632419d4ad1413
Rune Andersen <rune.andersen92@gmail.com>
2015-02-18 08:26:35 +0100

    Added the tmux config and cleaned up some old stuff

--- .swank.lisp
+++
@@ -1,2 +0,0 @@
-(in-package #:swank) 
-(defparameter *globally-redirect-io* t)
--- scripts/stopfan.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo chown rune /sys/kernel/debug
-sudo chown rune /sys/kernel/debug/vgaswitcheroo/switch
-echo OFF > /sys/kernel/debug/vgaswitcheroo/switch
