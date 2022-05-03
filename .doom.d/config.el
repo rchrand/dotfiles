6fb173fe2a7183b371095923684d081232830ed7
Rune Andersen <rune.andersen92@gmail.com>
2022-05-03 07:59:55 +0200

    Update .doom.d config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,17 +1,28 @@
-;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Rune Andersen"
+      user-mail-address "rune.andersen92@gmail.com")
+
+(after! rustic
+  (setq rustic-lsp-server 'rls))
 
 (setq doom-font (font-spec :family "Fira Code" :size 13))
 
-(setq flycheck-ruby-reek-executable "/Users/rchrand/.asdf/shims/reek")
-(setq flycheck-ruby-rubocop-executable "/Users/rchrand/.asdf/shims/rubocop")
-
 (map!
-  :gnvime "M-s" #'save-buffer
-
  ;; Easier window navigation
  :n "C-h"   #'evil-window-left
  :n "C-j"   #'evil-window-down
  :n "C-k"   #'evil-window-up
  :n "C-l"   #'evil-window-right)
+
+(setq doom-theme 'doom-material)
+
+(custom-set-faces `(region ((t (:background "#262626")))))
+
+(setq display-line-numbers-type t)
